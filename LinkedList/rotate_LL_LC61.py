0c109a65212a4f207633d59cdd10587a9872268f
Samarth Tiwari <150244619+SamarthTiwari16@users.noreply.github.com>
2026-03-24 22:46:40 +0530

    Create rotate_LL_LC61.py

---
+++ LinkedList/rotate_LL_LC61.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+        last = head
+        length = 1
+        while last.next:
+            last = last.next
+            length += 1
+        k = k % length
+        if k == 0:
+            return head
+        new_tail = head
+        for i in range(length - k - 1):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+        new_tail.next = None
+        last.next = head
+        return new_head
